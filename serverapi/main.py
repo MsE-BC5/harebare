1b36ea9376c4a8487bedbc32c38277aca42ff156
Reiko Matsuda <0ko.snow.world@gmail.com>
2023-12-25 14:19:23 +0900

    作業中の変更(ユーザー情報GET、POSTエンドポイント追加済み)をコミット

--- serverapi/main.py
+++ serverapi/main.py
@@ -5,16 +5,18 @@ from router import llm
 
 
 app = FastAPI()
+# ルーターをアプリケーションに含める
+app.include_router(api_router)
+
 app.include_router(llm.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # @app.get("/")
 # async def root():
