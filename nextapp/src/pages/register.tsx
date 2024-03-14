c381964ba6d59e452bd2716ffa6922eaccf448ff
ayaho-kunimatsu <aya.do9ll@gmail.com>
2024-03-14 13:35:33 +0900

    調整

--- nextapp/src/pages/register.tsx
+++ nextapp/src/pages/register.tsx
@@ -328,15 +328,6 @@ const handleFormSubmission = async (event: any) => {
         </form>
       </div>
       </div>
-      <footer className="bg-gray-100 text-center p-2 absolute bottom-0 w-full ">
-  <p className="text-sm text-gray-600">
-    © {new Date().getFullYear()} Harebare company. All rights reserved.
-  </p>
-  <a href="/privacy-policy" className="text-sm text-gray-600 hover:underline">
-    プライバシーポリシー
-  </a>
-</footer>
-      
     </>
   );
   };
